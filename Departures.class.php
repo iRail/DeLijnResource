4eb34f94b56f375d816f55b05b4e2c2748af64d5
Pieter Colpaert <pieter@iRail.be>
2012-05-30 18:06:04 +0200

    Fixed parameters trouble

--- Departures.class.php
+++ Departures.class.php
@@ -12,55 +12,55 @@ include_once('DeLijnStopTimesDao.php');
  
 class DeLijnDepartures extends AReader{
 
-	public function __construct($package, $resource, $RESTparameters) {
-		parent::__construct($package, $resource, $RESTparameters);
-		
-		$this->offset = 0;
-		$this->rowcount = 1024;
-	}
+    public function __construct($package, $resource, $RESTparameters) {
+        parent::__construct($package, $resource, $RESTparameters);		
+        $this->offset = 0;
+        $this->rowcount = 1024;
+    }
 
     public static function getParameters(){
-		return array("Offset" => "offset"
-					,"Rowcount" => "rowcount");
+        return array("stationid" => "Station ID that can be found in the Stations resource",
+                     "year" => "Year",
+                     "month" => "Month",
+                     "day" => "Day"
+                     ,"hour" => "Hour"
+                     ,"minute" => "Minute"
+                     ,"offset" => "Offset"
+                     ,"rowcount" => "Rowcount");
     }
 
     public static function getRequiredParameters(){
-		return array("Station ID" => "stationid",
-		"Year" => "year",
-		"Month" => "month",
-		"Day" => "day"
-		,"Hour" => "hour"
-		,"Minute" => "minute");
+        return array("stationid","year","month","day","hour","minute");
     }
 
     public function setParameter($key,$val){
         if ($key == "stationid"){
-			$this->stationid = $val;
-		} else if ($key == "year"){
-			$this->year = $val;
-		} else if ($key == "month"){
-			$this->month = $val;
-		} else if ($key == "day") {
-			$this->day = $val;
-		} else if ($key == "hour") {
-			$this->hour = $val;
-		} else if ($key == "minute") {
-			$this->minute = $val;
-		} else if ($key == "offset") {
-			$this->offset = $val;
-		} else if ($key == "rowcount") {
-			$this->rowcount = $val;
-		}
+            $this->stationid = $val;
+        } else if ($key == "year"){
+            $this->year = $val;
+        } else if ($key == "month"){
+            $this->month = $val;
+        } else if ($key == "day") {
+            $this->day = $val;
+        } else if ($key == "hour") {
+            $this->hour = $val;
+        } else if ($key == "minute") {
+            $this->minute = $val;
+        } else if ($key == "offset") {
+            $this->offset = $val;
+        } else if ($key == "rowcount") {
+            $this->rowcount = $val;
+        }
     }
 
     public function read(){
-		$stopTimesDao = new StopTimesDao();
+        $stopTimesDao = new StopTimesDao();
 	
-		return $stopTimesDao->getDepartures($this->stationid, $this->year, $this->month, $this->day, $this->hour, $this->minute, $this->offset, $this->rowcount);
+        return $stopTimesDao->getDepartures($this->stationid, $this->year, $this->month, $this->day, $this->hour, $this->minute, $this->offset, $this->rowcount);
     }
 
     public static function getDoc(){
-		return "This resource contains the Departures for a certain Station for a certain date and time from De Lijn.";
+        return "This resource contains the Departures for a certain Station for a certain date and time from De Lijn.";
     }
 }
 

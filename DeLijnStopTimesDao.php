482ad3988b61aa461bdb8033a0b77b614a8eb497
Pieter Colpaert <pieterc@member.fsf.org>
2012-11-13 18:05:49 +0100

    Added type of transit to De Lijn

--- DeLijnStopTimesDao.php
+++ DeLijnStopTimesDao.php
@@ -13,16 +13,16 @@ include_once('custom/packages/DeLijn/Config.class.php');
  
 class StopTimesDao {
 
-	public function __construct() {
-		R::setup(DeLijnConfig::$DB, DeLijnConfig::$DB_USER, DeLijnConfig::$DB_PASSWORD);
-	}
+    public function __construct() {
+        R::setup(DeLijnConfig::$DB, DeLijnConfig::$DB_USER, DeLijnConfig::$DB_PASSWORD);
+    }
 
-	/*
-	 *	Timezone set to Europe/Brussels
-	 */
-	var $timezone = "Europe/Brussels";
+    /*
+     *	Timezone set to Europe/Brussels
+     */
+    var $timezone = "Europe/Brussels";
 
-	private $GET_DEPARTURES_BY_NAME_QUERY = "SELECT DISTINCT route.route_short_name, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.departure_time_t
+    private $GET_DEPARTURES_BY_NAME_QUERY = "SELECT DISTINCT route.route_short_name, route.type, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.departure_time_t
 									FROM dlgtfs_stop_times times
 									JOIN dlgtfs_trips trip
 										ON trip.trip_id = times.trip_id
@@ -42,7 +42,7 @@ class StopTimesDao {
 									ORDER BY times.departure_time_t
 									LIMIT :offset, :rowcount;";
 									
-	private $GET_DEPARTURES_BY_ID_QUERY = "SELECT DISTINCT route.route_short_name, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.departure_time_t
+    private $GET_DEPARTURES_BY_ID_QUERY = "SELECT DISTINCT route.route_short_name, route.type, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.departure_time_t
 									FROM dlgtfs_stop_times times
 									JOIN dlgtfs_trips trip
 										ON trip.trip_id = times.trip_id
@@ -60,7 +60,7 @@ class StopTimesDao {
 									ORDER BY times.departure_time_t
 									LIMIT :offset, :rowcount;";
 									
-	private $GET_ARRIVALS_BY_NAME_QUERY = "SELECT DISTINCT route.route_short_name, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.arrival_time_t
+    private $GET_ARRIVALS_BY_NAME_QUERY = "SELECT DISTINCT route.route_short_name, route.type, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.arrival_time_t
 									FROM dlgtfs_stop_times times
 									JOIN dlgtfs_trips trip
 										ON trip.trip_id = times.trip_id
@@ -80,7 +80,7 @@ class StopTimesDao {
 									ORDER BY times.arrival_time_t
 									LIMIT :offset, :rowcount;";
 									
-	private $GET_ARRIVALS_BY_ID_QUERY = "SELECT DISTINCT route.route_short_name, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.arrival_time_t
+    private $GET_ARRIVALS_BY_ID_QUERY = "SELECT DISTINCT route.route_short_name, route.type, trip.trip_headsign, route.route_color, route.route_text_color, trip.direction_id, times.arrival_time_t
 									FROM dlgtfs_stop_times times
 									JOIN dlgtfs_trips trip
 										ON trip.trip_id = times.trip_id
@@ -98,117 +98,118 @@ class StopTimesDao {
 									ORDER BY times.arrival_time_t
 									LIMIT :offset, :rowcount;";
 																
-	/**
-	  *
-	  * @param int $stationName The Name of a station (Required)
-	  * @param int $year The Year (Required)
-	  * @param int $month The Month (Required)
-	  * @param int $day The Day (Required)
-	  * @param int $hour The Hour (Required)
-	  * @param int $minute The Minute (Required)
-	  * @return array A List of Departures for a given station, date and starting from a given time
-	  */
-	public function getDeparturesByName($stationName, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
-		date_default_timezone_set($this->timezone);
+    /**
+     *
+     * @param int $stationName The Name of a station (Required)
+     * @param int $year The Year (Required)
+     * @param int $month The Month (Required)
+     * @param int $day The Day (Required)
+     * @param int $hour The Hour (Required)
+     * @param int $minute The Minute (Required)
+     * @return array A List of Departures for a given station, date and starting from a given time
+     */
+    public function getDeparturesByName($stationName, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
+        date_default_timezone_set($this->timezone);
 		
-		$arguments = array(":stationName" => urldecode($stationName), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
-		$query = $this->GET_DEPARTURES_BY_NAME_QUERY;
+        $arguments = array(":stationName" => urldecode($stationName), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
+        $query = $this->GET_DEPARTURES_BY_NAME_QUERY;
 		
-		$result = R::getAll($query, $arguments);
-		return $this->parseStopTimes($result, $year, $month, $day);
-	}
+        $result = R::getAll($query, $arguments);
+        return $this->parseStopTimes($result, $year, $month, $day);
+    }
 	
-	/**
-	  *
-	  * @param int $stationId The ID of a station (Required)
-	  * @param int $year The Year (Required)
-	  * @param int $month The Month (Required)
-	  * @param int $day The Day (Required)
-	  * @param int $hour The Hour (Required)
-	  * @param int $minute The Minute (Required)
-	  * @return array A List of Departures for a given station, date and starting from a given time
-	  */
-	public function getDeparturesByID($stationId, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
-		date_default_timezone_set($this->timezone);
+    /**
+     *
+     * @param int $stationId The ID of a station (Required)
+     * @param int $year The Year (Required)
+     * @param int $month The Month (Required)
+     * @param int $day The Day (Required)
+     * @param int $hour The Hour (Required)
+     * @param int $minute The Minute (Required)
+     * @return array A List of Departures for a given station, date and starting from a given time
+     */
+    public function getDeparturesByID($stationId, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
+        date_default_timezone_set($this->timezone);
 		
-		$arguments = array(":stationId" => urldecode($stationId), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
-		$query = $this->GET_DEPARTURES_BY_ID_QUERY;
+        $arguments = array(":stationId" => urldecode($stationId), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
+        $query = $this->GET_DEPARTURES_BY_ID_QUERY;
 		
-		$result = R::getAll($query, $arguments);
-		return $this->parseStopTimes($result, $year, $month, $day);
-	}
+        $result = R::getAll($query, $arguments);
+        return $this->parseStopTimes($result, $year, $month, $day);
+    }
 	
-	/**
-	  *
-	  * @param int $stationName The Name of a station (Required)
-	  * @param int $year The Year (Required)
-	  * @param int $month The Month (Required)
-	  * @param int $day The Day (Required)
-	  * @param int $hour The Hour (Required)
-	  * @param int $minute The Minute (Required)
-	  * @return array A List of Arrivals for a given station, date and starting from a given time
-	  */
-	public function getArrivalsByName($stationName, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
-		date_default_timezone_set($this->timezone);
+    /**
+     *
+     * @param int $stationName The Name of a station (Required)
+     * @param int $year The Year (Required)
+     * @param int $month The Month (Required)
+     * @param int $day The Day (Required)
+     * @param int $hour The Hour (Required)
+     * @param int $minute The Minute (Required)
+     * @return array A List of Arrivals for a given station, date and starting from a given time
+     */
+    public function getArrivalsByName($stationName, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
+        date_default_timezone_set($this->timezone);
 		
-		$arguments = array(":stationName" => urldecode($stationName), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
-		$query = $this->GET_ARRIVALS_BY_NAME_QUERY;
+        $arguments = array(":stationName" => urldecode($stationName), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
+        $query = $this->GET_ARRIVALS_BY_NAME_QUERY;
 		
-		$result = R::getAll($query, $arguments);
-		return $this->parseStopTimes($result, $year, $month, $day);
-	}
+        $result = R::getAll($query, $arguments);
+        return $this->parseStopTimes($result, $year, $month, $day);
+    }
 	
-	/**
-	  *
-	  * @param int $stationId The ID of a station (Required)
-	  * @param int $year The Year (Required)
-	  * @param int $month The Month (Required)
-	  * @param int $day The Day (Required)
-	  * @param int $hour The Hour (Required)
-	  * @param int $minute The Minute (Required)
-	  * @return array A List of Arrivals for a given station, date and starting from a given time
-	  */
-	public function getArrivalsById($stationId, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
-		date_default_timezone_set($this->timezone);
+    /**
+     *
+     * @param int $stationId The ID of a station (Required)
+     * @param int $year The Year (Required)
+     * @param int $month The Month (Required)
+     * @param int $day The Day (Required)
+     * @param int $hour The Hour (Required)
+     * @param int $minute The Minute (Required)
+     * @return array A List of Arrivals for a given station, date and starting from a given time
+     */
+    public function getArrivalsById($stationId, $year, $month, $day, $hour, $minute, $offset=0, $rowcount=1024) {	
+        date_default_timezone_set($this->timezone);
 		
-		$arguments = array(":stationId" => urldecode($stationId), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
-		$query = $this->GET_ARRIVALS_BY_ID_QUERY;
+        $arguments = array(":stationId" => urldecode($stationId), ":year" => urldecode($year), ":month" => urldecode($month), ":day" => urldecode($day), ":hour" => urldecode($hour), ":minute" => urldecode($minute), ":offset" => intval(urldecode($offset)), ":rowcount" => intval(urldecode($rowcount)));
+        $query = $this->GET_ARRIVALS_BY_ID_QUERY;
 		
-		$result = R::getAll($query, $arguments);
-		return $this->parseStopTimes($result, $year, $month, $day);
-	}
+        $result = R::getAll($query, $arguments);
+        return $this->parseStopTimes($result, $year, $month, $day);
+    }
 	
-	/**
-	  * Parses the data that comes out of the queries and creates nice objects that could be returned to the user.
-	  */
-	private function parseStopTimes($result, $year, $month, $day) {
-		$stoptimes = array();
-		foreach($result as &$row){
-			$stoptime = array();
+    /**
+     * Parses the data that comes out of the queries and creates nice objects that could be returned to the user.
+     */
+    private function parseStopTimes($result, $year, $month, $day) {
+        $stoptimes = array();
+        foreach($result as &$row){
+            $stoptime = array();
 			
-			$stoptime["short_name"] = $row["route_short_name"];
-			$stoptime["long_name"] = $row["trip_headsign"];
-			$stoptime["color"] = $row["route_color"];
-			$stoptime["text_color"] = $row["route_text_color"];
-			$stoptime["direction"] = $row["direction_id"];
+            $stoptime["short_name"] = $row["route_short_name"];
+            $stoptime["type"] = $row["route.type"];
+            $stoptime["long_name"] = $row["trip_headsign"];
+            $stoptime["color"] = $row["route_color"];
+            $stoptime["text_color"] = $row["route_text_color"];
+            $stoptime["direction"] = $row["direction_id"];
 
-			if(isset($row["departure_time_t"])) {
-				$timeString =  $row["departure_time_t"];
-			} else {
-				$timeString =  $row["arrival_time_t"];
-			}
+            if(isset($row["departure_time_t"])) {
+                $timeString =  $row["departure_time_t"];
+            } else {
+                $timeString =  $row["arrival_time_t"];
+            }
 			
-			$split = explode(':', $timeString);
-			$hour = $split[0];
-			$minute = $split[1];
+            $split = explode(':', $timeString);
+            $hour = $split[0];
+            $minute = $split[1];
 			
-			$date = mktime($hour, $minute, 0, $month, $day, $year);
-			$stoptime["iso8601"] = date("c", $date);
-			$stoptime["time"] = date("U", $date);
+            $date = mktime($hour, $minute, 0, $month, $day, $year);
+            $stoptime["iso8601"] = date("c", $date);
+            $stoptime["time"] = date("U", $date);
 			
-			$stoptimes[] = $stoptime;
-		}
+            $stoptimes[] = $stoptime;
+        }
 		
-		return $stoptimes;
-	}
+        return $stoptimes;
+    }
 }
